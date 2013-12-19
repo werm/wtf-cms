9fba7f23b943d38bb6c6ed790d3197d64cbeb681
Craig Wermert <craig.wermert@osumc.edu>
2013-12-19 10:54:53 -0500

    Sanitizing inputs

--- app.rb
+++ app.rb
@@ -11,39 +11,58 @@ class Post < ActiveRecord::Base
  validates :body, presence: true
 end
 
-get "/" do
-  @posts = Post.order("created_at DESC")
-  @title = "Hi."
-  erb :"posts/index"
-end
-
 helpers do
+
+  include Rack::Utils
+  alias_method :h, :escape_html
+
   def title
     if @title
       "#{@title}"
     else
-      "Hi."
+      "Welcome."
     end
+  end
+
+end # /helpers
+
+# get ALL posts
+get "/" do
+  @posts = Post.order("created_at DESC")
+  @title = "Welcome."
+  erb :"posts/index"
+end
+
+# create new post
+get "/posts/create" do
+  @title = "Create post"
+  @post = Post.new
+  erb :"posts/create"
+end
+post "/posts" do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect "posts/#{@post.id}", :notice => 'Congrats! Love the new post. (This message will disapear in 4 seconds.)'
+  else
+    redirect "posts/create", :error => 'Something went wrong. Try again. (This message will disapear in 4 seconds.)'
   end
 end
 
-get "/posts/create" do
- @title = "Create post"
- @post = Post.new
- erb :"posts/create"
-end
-
-post "/posts" do
- @post = Post.new(params[:post])
- if @post.save
-   redirect "posts/#{@post.id}", :notice => 'Congrats! Love the new post. (This message will disapear in 4 seconds.)'
- else
-   redirect "posts/create", :error => 'Something went wrong. Try again. (This message will disapear in 4 seconds.)'
- end
-end
-
+# view post
 get "/posts/:id" do
- @post = Post.find(params[:id])
- @title = @post.title
- erb :"posts/view"
+  @post = Post.find(params[:id])
+  @title = @post.title
+  erb :"posts/view"
+end
+
+# edit post
+get "/posts/:id/edit" do
+  @post = Post.find(params[:id])
+  @title = "Edit Form"
+  erb :"posts/edit"
+end
+put "/posts/:id" do
+  @post = Post.find(params[:id])
+  @post.update(params[:post])
+  redirect "/posts/#{@post.id}"
 end
