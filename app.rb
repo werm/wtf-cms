86652413a8220c4dabecc6c06bba04659da38e44
Craig Wermert <craig.wermert@osumc.edu>
2013-12-19 10:38:29 -0500

    First commit

---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require './environment'
+
+
+class Post < ActiveRecord::Base
+
+end
---
+++ environment.rb
@@ -0,0 +1,17 @@
+configure :development do
+ set :database, 'sqlite:///dev.db'
+ set :show_exceptions, true
+end
+
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'mysql:///localhost/ruby_cms')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'mysql' ? 'mysql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
